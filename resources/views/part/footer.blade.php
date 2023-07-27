8ce1f51d7e2b85917d82e8e2fb65983603f56da2
muhammadalvin92 <alvinbomber87@gmail.com>
2023-07-27 12:12:21 +0700

    Part Footer Responsive

--- resources/views/part/footer.blade.php
+++ resources/views/part/footer.blade.php
@@ -1,1 +1,43 @@
-<link rel="stylesheet" href=" {{ asset('../css/part/navbar.css')}}">
+<link rel="stylesheet" href=" {{ asset('../css/part/footer.css')}}">
+
+<footer class="pt-5 pb-4" style="margin-top: 7rem;">
+    <div class="container">
+        <div class="row justify-content-md-between align-items-start">
+            <div class="col-lg-2 col-md-2 footerLogo">
+                    <img src="{{asset('../image/pdfiLogoEllipse.png')}}" alt="">
+            </div>
+            <div class="col-lg-3 col-md-3 footerTitle">
+                <p class="xl fw-bold my-2">PERSATUAN DOKTER FORENSIK INDONESIA</p>
+                <p class="text-justify my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero sapiente alias nam laborum? At tempora dolores architecto eligendi.</p>
+                <div class="sosmed d-flex align-items-center justify-content-center justify-content-md-start gap-3 my-3">
+                    <a href=""><i class="fa-brands fa-facebook-f" style="color: #ffffff;"></i></a>
+                    <a href=""><i class="fa-brands fa-twitter" style="color: #ffffff;"></i></a>
+                    <a href=""><i class="fa-brands fa-instagram" style="color: #ffffff;"></i></a>
+                    <a href=""><i class="fa-brands fa-youtube" style="color: #ffffff;"></i></a>
+                    <a href=""><i class="fa-brands fa-tiktok" style="color: #ffffff;"></i></a>
+                    <a href=""><i class="fa-solid fa-circle-play" style="color: #ffffff;"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-8 col-7">
+                <p class="xl fw-bold my-2">ALAMAT</p>
+                <p class="text-justify my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci similique earum odit iste quo cum incidunt inventore! Saepe.</p>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-5">
+                <p class="xl fw-bold mt-2">KONTAK KAMI</p>
+                <div class="email d-flex align-items-center gap-2 mt-2">
+                    <i class="fa-solid fa-envelope" style="color: #ffffff;"></i>
+                    <p>pdfi@gmail.co.id</p>
+                </div>
+                <div class="email d-flex align-items-center gap-2">
+                    <i class="fa-brands fa-whatsapp" style="color: #ffffff;"></i>
+                    <p>021 - 2569 - 5632</p>
+                </div>
+            </div>
+        </div>
+        <div class="hr"></div>
+        <div class="bottomFooter d-flex justify-content-between">
+            <p>©2020 PDFI, All Rights Reserved.</p>
+            <p>Terms & Condition - Privacy - Cookies</p>
+        </div>
+    </div>
+</footer>
